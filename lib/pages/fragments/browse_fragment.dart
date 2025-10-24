ba70efa9e0ecf3f168ce09397d129b2c510d31ad
Yudi Alfarizi <yudi.alfaarizi@gmail.com>
2025-10-24 22:14:36 +0700

    Update Browse Unggulan

--- lib/pages/fragments/browse_fragment.dart
+++ lib/pages/fragments/browse_fragment.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:ngibrit_in/controllers/browse_featured_controller.dart';
 import 'package:ngibrit_in/models/bike.dart';
 import 'package:extended_image/extended_image.dart';
@@ -65,13 +67,10 @@ class _BrowseFragmentState extends State<BrowseFragment> {
           String status = browseFeaturedController.status;
           if (status == '') return const SizedBox();
           if (status == 'loading') {
-            return const Center(
-              child: CircularProgressIndicator()
-            );
+            return const Center(child: CircularProgressIndicator());
           }
           if (status != 'success') {
-            return Center(
-              child: FailedUi(message: status));
+            return Center(child: FailedUi(message: status));
           }
           List<Bike> list = browseFeaturedController.list;
 
@@ -83,8 +82,8 @@ class _BrowseFragmentState extends State<BrowseFragment> {
               itemBuilder: (context, index) {
                 Bike bike = list[index];
                 final margin = EdgeInsets.only(
-                  left: index==0?24:12,
-                  right: index==list.length-1?24:12,
+                  left: index == 0 ? 24 : 12,
+                  right: index == list.length - 1 ? 24 : 12,
                 );
                 bool isTrending = index == 0;
                 return buildItemFeatured(bike, margin, isTrending);
@@ -113,13 +112,102 @@ class _BrowseFragmentState extends State<BrowseFragment> {
         children: [
           Stack(
             children: [
-              ExtendedImage.network(
-                bike.image,
-                width: 220,
-                height: 170,
+              ExtendedImage.network(bike.image, width: 220, height: 170),
+              if (isTrending)
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffFF2055),
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 4),
+                        blurRadius: 10,
+                        color: const Color(0xffFF2055).withValues(alpha: 0.5),
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 6, horizontal: 14),
+                  child: const Text(
+                    'Trending',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          const Spacer(),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Text(
+                      bike.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xff070623),
+                      ),
+                    ),
+                    const Gap(4),
+                    Text(
+                      bike.category,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff838384),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              RatingBar.builder(
+                initialRating: bike.rating.toDouble(),
+                itemPadding: const EdgeInsets.all(0),
+                itemSize: 16,
+                unratedColor: Colors.grey[300],
+                allowHalfRating: true,
+                itemBuilder: (context, index) => Icon(Icons.star, color: Color(0xffFFBC1C),),
+                ignoreGestures: true,
+                onRatingUpdate: (value){},
+              ),
+              
+            ],
+          ),
+          const Gap(16),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                NumberFormat.currency(
+                  decimalDigits: 0,
+                  locale: 'id_ID',
+                  symbol: 'Rp ',
+                ).format(bike.price),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff6747E9),
+                ),
+              ),
+              const Text(
+                '/hari',
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff838384),
+                ),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
